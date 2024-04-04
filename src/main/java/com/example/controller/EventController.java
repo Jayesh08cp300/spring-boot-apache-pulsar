d220b5ebf1c6f640bf9933249fc02c39e6d1bb7b
prajapatij <jayesh.prajapati@pmcretail.com>
2024-04-04 14:45:11 +0530

    example of apache pulsar

---
+++ src/main/java/com/example/controller/EventController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import com.example.model.Customer;
+import com.example.producer.EventPublisher;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/producer")
+public class EventController {
+
+	@Autowired
+	private EventPublisher publisher;
+
+	@GetMapping("/text/{message}")
+	public String sendTextEvent(@PathVariable String message) throws PulsarClientException {
+		publisher.publishPlainMessage(message);
+		return "message published !";
+	}
+
+	@PostMapping("/raw")
+	public String sendRawEvent(@RequestBody Customer customer) throws PulsarClientException {
+		publisher.publishRawMessage(customer);
+		return "Custom object published !";
+	}
+}
